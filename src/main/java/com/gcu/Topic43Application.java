896afc606b154e36c93073d93a51455328d45ed5
Ulovebj <124942069+Hoodrichbj@users.noreply.github.com>
2025-04-06 13:03:25 -0700

    working code

--- src/main/java/com/gcu/Topic43Application.java
+++ src/main/java/com/gcu/Topic43Application.java
@@ -2,14 +2,11 @@ package com.gcu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({ "com.gcu"})
 public class Topic43Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Topic43Application.class, args);
 	}
-
-} 
+}
